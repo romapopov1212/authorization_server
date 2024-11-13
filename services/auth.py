a43635da4dcf735de7d642cb8f0dc136aac48423
tutik77 <tutikosnov@gmail.com>
2024-11-14 01:15:48 +0300

    add login (idk mb it works)

--- services/auth.py
+++ services/auth.py
@@ -93,3 +93,13 @@ class AuthService:
                 },
             )
         return self.create_token(user)
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, settings.jwt_secret, algorithms=[settings.jwt_algorithm])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        return user_id
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
