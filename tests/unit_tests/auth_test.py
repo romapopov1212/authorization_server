a5d20477575dd9ae2a159afddd0c0ae3c1ed821f
Popov Roman <popovrromma@gmail.com>
2024-12-08 01:12:54 +1000

    add login test, fix clear database after test (more one test in one file while doesnt work)

--- tests/unit_tests/auth_test.py
+++ tests/unit_tests/auth_test.py
@@ -1,14 +1,19 @@
 from contextlib import nullcontext as does_not_raise
-
+from unittest.mock import AsyncMock, Mock
 import pytest
 from sqlalchemy.future import select
-
+from services.token import TokenService
 import models
+from models.auth import Token
 from services.auth import AuthService
 from db_setup import User
 
+class MockTokenService:
+    def create_access_token(self, data):
+        return "mock_access_token"
+
 class TestAuthService:
-    
+
     @pytest.mark.parametrize(
         "email, username, password, phone_number, expectation",
         [
@@ -27,25 +32,49 @@ class TestAuthService:
             )
         assert True
 
+    # 
+    # @pytest.mark.asyncio
+    # async def test_register(self, setup_database, session):
+    #     service = AuthService(session=session)
+    #
+    #     user_data = models.auth.UserRegistration(
+    #         email="test@test.com",
+    #         username="tutik77",
+    #         password="AAaa1234!!",
+    #         phone_number="89528723878",
+    #     )
+    #
+    #     result = await service.register(user_data)
+    #
+    #     assert result.status_code == 201
+    #
+    #     db_user = await session.execute(
+    #         select(User).filter(User.email == user_data.email)
+    #     )
+    #     db_user = db_user.scalars().first()
+    #
+    #     assert db_user is not None
 
     @pytest.mark.asyncio
-    async def test_register(self, setup_database, session):
-        service = AuthService(session=session)
-
-        user_data = models.auth.UserRegistration(
+    async def test_login(self, setup_database, session):
+        token_service = TokenService()
+        service = AuthService(session=session, token_service=token_service)
+        user_data_to_register = models.auth.UserRegistration(
             email="test@test.com",
             username="tutik77",
             password="AAaa1234!!",
             phone_number="89528723878",
         )
-        
-        result = await service.register(user_data)
-        
-        assert result.status_code == 201
+        await service.register(user_data_to_register)
+        user = await service.get_user_by_email(user_data_to_register.email)
+        user.is_active = True
+        session.add(user)
+        await session.commit()
+        email = "test@test.com"
+        password = "AAaa1234!!"
+        code = "string"
 
-        db_user = await session.execute(
-            select(User).filter(User.email == user_data.email)
-        )
-        db_user = db_user.scalars().first()
-
-        assert db_user is not None
+        result = await service.authenticate_user(email, password, code)
+        assert result is not None
+        assert isinstance(result, Token)
+        #assert result.status_code == 200
